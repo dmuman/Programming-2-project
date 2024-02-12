2488b29bddcec8a6aceef39a5ee7feb6385b6edb
dmuman <umanskyi.dmytro@gmail.com>
2024-02-12 12:57:50 +0000

    ф

--- DoctorCollectionTest.py
+++ DoctorCollectionTest.py
@@ -1,7 +1,9 @@
+from DoctorTest import *
+
 class DoctorCollectionTest:
     def __init__(self, filename):
         self._filename = filename
-        self._header, self._doctors = self.readFile()
+        self._header, self._doctorsData = self.readFile()
 
     def getFilename(self):
         return self._filename
@@ -17,17 +19,26 @@ class DoctorCollectionTest:
                 headerTemp = line.rstrip('\n')
                 header.append(headerTemp)
 
-            doctors = [line.strip().split(", ") for line in lines[7:] if line.strip()]
+            doctorsData = [line.strip().split(", ") for line in lines[7:] if line.strip()]
 
-        return header, doctors
+        return header, doctorsData
     
     def getHeader(self):
         return self._header
 
-    def getDoctors(self):
-        return self._doctors
+    def getDoctorsData(self):
+        return self._doctorsData
     
+    def getDoctors(self):
+        doctors = []
+        for data in self.getDoctorsData():
+            name, skill, nextFreeHours, dailyMinutes, weeklyHours = data
+            doctor = DoctorTest(name, int(skill), nextFreeHours, int(dailyMinutes), weeklyHours)
+            doctors.append(doctor)
 
+        return doctors
+    
 doctors = DoctorCollectionTest('doctors10h00.txt')
 print(doctors.getHeader())
+print(doctors.getDoctorsData())
 print(doctors.getDoctors())

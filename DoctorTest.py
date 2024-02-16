dece8f3471ca9db7652c5b51b1b7d940e517166f
dmuman <umanskyi.dmytro@gmail.com>
2024-02-16 12:11:48 +0000

    updating doctors times

--- DoctorTest.py
+++ DoctorTest.py
@@ -81,8 +81,71 @@ class DoctorTest:
     def isDoctorFree(self):
         pass
 
-    def updateDoctorsTime(self):
-        pass
+    def updateDoctorsTime(self, minutesToAdd):
+        oldFreeHours = self.getNextFreeHours()
+        oldDailyMinutes = self.getDailyMinutes()
+        oldWeeklyHours = self.getWeeklyHours()
+
+        #declaring variables for new free hours, daily minutes worked and weekly hours worked
+        newFreeHours = None
+        newWeeklyHours = None
+        newDailyMinutes = int(oldDailyMinutes) + minutesToAdd #already adding minutes to it
+
+        #declaring variables for old hours and minutes from old free hours
+        hoursFromOldHours, minutesFromOldHours = self.timeToInt(oldFreeHours)
+
+        if minutesFromOldHours < 30:                                            #checks if minutes from old hours are less then 30(time from the new schedule)
+            minutesFromOldHours = self.timeToInt(oldFreeHours)[1] + \
+                (30 - self.timeToInt(oldFreeHours)[1])                               #if so, properly adding new minutes
+            oldFreeHours = self.intToTime(hoursFromOldHours, minutesFromOldHours)    #updating old free hours, considering the time in new schedule
+            newFreeHours = updateHours(oldFreeHours, minutesToAdd)              #calculating new free hours, using updateHours() function from the dateTime module
+        
+        if int(oldMinutesDias) < 240:                                       #checks if old daily minutes worked are less than 240
+            if newMinutesDias >= 240:                                       #if so, checks if new daily minutes worked are greater than 240
+                newFreeHours = intToTime(hourToInt(oldFreeHours) + 1, \
+                                        minutesToInt(oldFreeHours)+20)     #if so, adding one hour(i.e. one hour pause) to the new free hours
+            else:                                                           #checks if new daily minutes worked are less than 240
+                newFreeHours = updateHours(oldFreeHours, minutesToAdd)      #if so, adding minutes to the new free hours, using updateHours() function from th dateTime module
+        else:                                                               #checks if old daily minutes worked are already greater than 240
+            newFreeHours = updateHours(oldFreeHours, minutesToAdd)          #if so, adding minutes to the new free hours, using updateHours() function from th dateTime module
+        
+        newHorasSemanais = updateHours(oldHorasSemanais, minutesToAdd)      #updating new weekly worked hours using the updateHours() function from the dateTime module
+
+        if hourToInt(newHorasSemanais) >= 40:           #checks if new weekly worked hours are greater than 40
+            newFreeHours = WKL_PAUSE                    #if so, doctor receives weekly pause   
+
+        return [oldDoctor[DOCT_NAME_IDX], str(oldDoctor[DOCT_SKILL_IDX]), \
+                newFreeHours, str(newMinutesDias), newHorasSemanais]
+
+    def updateHours(hoursToUpdate, minutesToAdd):
+        #declaring variables for holding the int representatin of hours, minutes and total minutes
+        intHours = None 
+        intMinutes = None
+        totalMinutes = None
+
+        #declaring variable for the new, updated hours
+        updatedHours = None
+
+        #in case new minutes are greater than 60
+        if minutesToInt(hoursToUpdate) + minutesToAdd > 60:
+            intHours = hourToInt(hoursToUpdate)                                 #assigning hours
+            intMinutes = minutesToInt(hoursToUpdate)                            #assigning minutes
+            totalMinutes = intHours * 60 + intMinutes + minutesToAdd            #new totalMinutes
+            updatedHours = intToTime((totalMinutes // 60), (totalMinutes % 60)) #new, updated hours, based on totalMinutes
+
+        #in case new minutes are equal 60
+        elif minutesToInt(hoursToUpdate) + minutesToAdd == 60:
+            intHours = hourToInt(hoursToUpdate) + 1         #updating hours(adding 1 hour)
+            intMinutes = 0                                  #updating minutes(equal 00)
+            updatedHours = intToTime(intHours, intMinutes)  #new, updated hours
+
+        #in case new minutes are less than 60
+        else:
+            intHours = hourToInt(hoursToUpdate)                     #assigning hours. they remain the same
+            intMinutes = minutesToInt(hoursToUpdate) + minutesToAdd #updating minutes with minutesToAdd
+            updatedHours = intToTime(intHours, intMinutes)          #new, updated hours
+
+        return updatedHours
 
     def updateDoctors(self):
         pass
